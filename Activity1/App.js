ad1478d94b43743522b73c4354ec8fed7ce3a0f0
vitug09 <jerronjessevitug09@gmail.com>
2025-02-06 16:48:24 +0800

    ScrollView

---
+++ Activity1/App.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import { ScrollView, Text} from 'react-native';
+
+const App = () => {
+  return (
+  <ScrollView>
+    <Text> Ikaw na kaya siguro ang pangakong paraiso
+Naglayag sa kawalan di huminto kahit pawis to
+Napansin ko sa pagibig tanga ka kahit alisto
+Sinangla ko pati puso daig pa ang nag-casino
+Kalimutan ang kahapon di pe-pwede 'yon sa akin (di pe-pwede 'yon sa akin)
+Natutunan kong pumulot ng sarap sa nagpasakit
+Ang balakid na humarang walang dating sa 'king hangarin
+Masaya sa naranasan kahit hindi man papalarin
+One two three four
+Kaya sa pagtayo ko sa umaga ay hanap ko na
+Ngiti mo na nagpasaya
+Kakaiba nagbago na lahat lahat salamat at
+Natagpuan kita (natagpuan kita)
+Natagpuan kita (natagpuan kita)
+Natagpuan kita (natagpuan kita)
+Natagpuan kita (natagpuan kita)
+Natagpuan kita (hoh)
+Maliban sa 'king nanay ikaw lang tumawag sa 'king gwapo
+Kahit malabo ang iyong mata
+Ako'y dakila mo na taga suyo paglingkuran pagsilbihan
+Kahit pareho tayong alagain na
+Pero alam mo ba ako sayo sobrang tuwa
+Naka-order ka sa fast food ng ikaw lang mag isa
+Isang problema lang kanina ginawa mo nang lima
+Dibale na basta hindi mo kailanman pagpapaglit to sa iba
+Sasagipin kita ako ang superhero sayo lang ako magbibida
+Ikaw pa din ang maganda sa paningin walang iba (walang iba)
+One two three four
+Kaya sa pagtayo ko sa umaga ay hanap ko na
+Ngiti mo na nagpasaya
+Kakaiba nagbago na lahat lahat salamat at
+Natagpuan kita (natagpuan kita)
+Natagpuan kita (natagpuan kita)
+Natagpuan kita (natagpuan kita)
+Natagpuan kita (natagpuan kita)
+Natagpuan kita
+Ikaw lamang pipiliin talaga
+Dahil mas gusto mong mag ukay kaysa na mag SM pa (ikaw lang)
+Ikaw lamang pipiliin talaga
+Dahil gusto mong manatili kahit na mahirap na
+Alam mo kase kapag kasama ka
+Lipad lang sa ulap pansamantala
+Mukhang si kupido ang pumana sa
+Akin pero baril ang dala niya
+Walang dala (walang dala)
+Walang dala walang dala walang dala (walang dala)
+Walang dala (walang dala)
+Walang dala walang dala
+Kaya sa pagtayo ko sa umaga ay hanap ko na
+Ngiti mo na nagpasaya
+Kakaiba nagbago na lahat lahat salamat at
+Natagpuan kita (natagpuan kita)
+Natagpuan kita (natagpuan kita)
+Natagpuan kita (natagpuan kita)
+Natagpuan kita (natagpuan kita)
+Natagpuan kita
+Gusto ko ako lang gusto
+Gusto ko ako lang gusto
+Gusto ko ako lang gusto
+Gusto ko ako lang ang gusto (hey)
+Gusto ko ako lang gusto (hey oh)
+Gusto ko ako lang ang gusto mo (gusto ako lang gusto hey)
+Gusto ko ako lamang gusto mo ang gusto ko (gusto ko ako lang ang gusto hey)
+Gusto ko ako lamang gusto mo (gusto ko ako lang ang gusto hey)
+Gusto ko ako lang ang gusto mo </Text>
+
+    <Text> </Text>
+
+    </ScrollView>
+  );
+};
+export default App;
+  
